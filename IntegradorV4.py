8a7ff3715c846eef4de46377960cf22972032181
gerardoun <gdurdaneta@gmail.com>
2021-02-22 23:04:13 -0300

    Update IntegradorV4.py

--- IntegradorV4.py
+++ IntegradorV4.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Button, Entry, IntVar, Label, StringVar, ttk
+from tkinter import Button, Entry, IntVar, Label, StringVar, ttk, scrolledtext 
 from tkinter import LabelFrame, Tk, messagebox
 import sqlite3
 
@@ -24,7 +24,8 @@ def creaddb():
 def altadb():
     creaddb()
     cursor = conectadb()
-    alta = cursor.execute("INSERT INTO PALERMO('interno, tipo, equu, ubicapatch, ssdatos, usuario') VALUES (?,?,?,?,?,?)")
+    sql = "insert into PALERMO (interno, tipo, equu, ubicapatch, ssdatos, usuario) values (?,?,?,?,?,?)"
+    alta = cursor.execute(sql,datos)
     cursor.commit()
     cursor.close()
     return alta
@@ -54,80 +55,81 @@ def modificadb():
     cursor.close()
 
 
-#interno = IntVar()
-def ventana():
-    
-    
-    conectadb()
+
+
+conectadb()
    
     
-    #Ventana Principal
-    ventana = Tk()
-    ventana.geometry("800x600")
-    ventana.title("Integrador V4")
-    #******************************
+#Ventana Principal
+ventana = Tk()
+ventana.geometry("800x400")
+ventana.title("Integrador V4")
+#******************************
 
-    interno = IntVar()
-    tipo = StringVar()
-    patchera = StringVar()
-    equu = StringVar()
-    patchera = StringVar()
-    ssdatos = StringVar()
+datos = []
+interno = IntVar()
+tipo = StringVar()
+patchera = StringVar()
+equu = StringVar()
+usuario = StringVar()
+ssdatos = StringVar()
 
+"""datos.append(interno+","+
+            tipo+","+
+            patchera+","+
+            equu+","+
+            ssdatos+","+
+            usuario)"""
 
-    interno = interno.get()
-    tipo = tipo.get()
-    equu = equu.get()
-    patchera = patchera.get()
-    ssdatos = ssdatos.get()
+interno = interno.get()
+tipo = tipo.get()
+equu = equu.get()
+patchera = patchera.get()
+ssdatos = ssdatos.get()
 
-    #Pannel de pestañas
-    pestaña = ttk.Notebook(ventana)
-    p1 = ttk.Frame(pestaña)   # first page, which would get widgets gridded into it
-    p2 = ttk.Frame(pestaña)   # second page
-    pestaña.add(p1, text='Consulta')
-    pestaña.add(p2, text='Modificar')
-    pestaña.grid(column= 0, row=0)
-    
-    sInterno = Label(p1, text="Interno")
-    sInterno.grid(column=0, row=0, padx=0, pady=0)
-    sInternoEntry = Entry(p1, textvariable=interno)
-    sInternoEntry.grid(column=1 , row=0)
+#Pannel de pestañas
+pestaña = ttk.Notebook(ventana)
+p1 = ttk.Frame(pestaña)   # first page, which would get widgets gridded into it
+p2 = ttk.Frame(pestaña)   # second page
+pestaña.add(p1, text='Consulta')
+pestaña.add(p2, text='Modificar')
+pestaña.grid(column= 0, row=0)
 
-    sTipo = Label(p1, text="Tipo")
-    sTipo.grid(column=0, row=1, padx=0, pady=0)
-    sTipoEntry = Entry(p1, textvariable=tipo)
-    sTipoEntry.grid(column=1 , row=1)
+#Penstaña Consulta
+sInterno = Label(p1, text="Interno")
+sInterno.grid(column=0, row=0, padx=0, pady=0)
+sInternoEntry = Entry(p1, textvariable=interno)
+sInternoEntry.grid(column=1 , row=0)
 
-    sEquu = Label(p1, text="Equu")
-    sEquu.grid(column=0, row=2, padx=0, pady=0)
-    sEquuEntry = Entry(p1, textvariable=equu)
-    sEquuEntry.grid(column=1 , row=2)
+sTipo = Label(p1, text="Tipo")
+sTipo.grid(column=0, row=1, padx=0, pady=0)
+sTipoEntry = Entry(p1, textvariable=tipo, state="readonly")
+sTipoEntry.grid(column=1 , row=1)
 
-    sPatchera = Label(p1, text="Patchera")
-    sPatchera.grid(column=0, row=3, padx=0, pady=0)
-    sPatcheraEntry = Entry(p1, textvariable=patchera)
-    sPatcheraEntry.grid(column=1 , row=3)
-    
-    varSdDatos = Label(p1, text="Sala de datos")
-    varSdDatos.grid(column=0, row=4, padx=0, pady=0)
-    varSdDatosEntry = Entry(p1, textvariable=ssdatos)
-    varSdDatosEntry.grid(column=1 , row=4)
-    
-    ingresa = Button(p1, text="Consulta", command=altadb())
-    ventana.mainloop()
+sEquu = Label(p1, text="Equu")
+sEquu.grid(column=0, row=2, padx=0, pady=0)
+sEquuEntry = Entry(p1, textvariable=equu, state="readonly")
+sEquuEntry.grid(column=1 , row=2)
 
-ventana()
+sPatchera = Label(p1, text="Patchera")
+sPatchera.grid(column=0, row=3, padx=0, pady=0)
+sPatcheraEntry = Entry(p1, textvariable=patchera, state="readonly")
+sPatcheraEntry.grid(column=1 , row=3)
 
+varSdDatos = Label(p1, text="Sala de datos")
+varSdDatos.grid(column=0, row=4, padx=0, pady=0)
+varSdDatosEntry = Entry(p1, textvariable=ssdatos, state="readonly")
+varSdDatosEntry.grid(column=1 , row=4)
 
-
-
-
-
-
-
+mostrarlabelframe = ttk.Labelframe(p1,text="Consulta")
+mostrar = scrolledtext(mostrarlabelframe, width=30, height=10)
 
 
+
+consulta = Button(p1, text="Consulta")
+consulta.grid(column=1, row=7)
+
+ventana.mainloop()
 
 
 
